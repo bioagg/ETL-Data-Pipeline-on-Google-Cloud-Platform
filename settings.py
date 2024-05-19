e1bf8cf9c22569b9a9a10c445fee8442c3716139
bioagg <bioaagg@gmail.com>
2024-05-19 13:05:49 +0300

    change settings

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 # This variable will be True only when running the code from our local machine
 is_local_environment = True # Set to False before deploying to Cloud Functions
 
-cloud_storage_service_account = "./Keys/cloud-storage-admin-service-account.json"
-bigquery_service_account = "./Keys/bigquery-admin-service-account.json"
+is_local_environment = True # Set to False before deploying to Cloud Functions
+cloud_storage_service_account = "" # Replace with your Cloud Storage service account filename
+bigquery_service_account = "" # Replace with your BigQuery service account filename
 
-api_key = 'fb598271fef4c3b6cb0030c61751e2d7'
-project_id = 'weather-api-data-analysis'
-dataset_id= 'weather_api'
-bucket_name = 'data_api_weather'
+api_key = '' # Replace with your OpenWeather API key
+project_id = '' # Replace with your GCP Project ID
+dataset_id= '' # Replace with your BigQuery Dataset Name
